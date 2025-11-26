be73ca78a6a5a4e84e26bb621262c67bd264f3f4
James Dennis <emarketguys27@gmail.com>
2025-11-26 20:03:47 +0100

    tweaks to styling

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,7 +1,11 @@
 const ctaHeading = document.querySelector(".cta-wrapper-index h2");
 const ctaText = document.querySelector(".cta-wrapper-index p");
 const ctaBtns = gsap.utils.toArray("buttons .hire-button");
+const listItems = document.querySelectorAll(".problem-list .main-li");
 
+listItems.forEach(item => {
+                gsap.set(item, { x: 30, opacity: 0 });
+            })
 let ctaTl = gsap.timeline({
     scrollTrigger: {
         trigger: ".cta-wrapper-index",
@@ -29,3 +33,13 @@ ctaTl.from(ctaHeading, {
     scale: 0,
     duration: .8,
 }, "<")
+.to(listItems, {
+    x: 0, opacity: 1, duration: .5, ease: "expo.inOut", stagger: .35,
+}, ">")
+.from(".action-block h4", {
+    y: 30, opacity: 0, duration: 1.5, ease: "bounce.out", delay: 1.3,
+}, ">-.15")
+.from(".action-block buttons", {
+    y: 30, opacity: 0, duration: .5, ease: "bounce.out",
+}, ">-.5")
+            
